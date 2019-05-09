7e9058a2bef97460f6b027724f91e24a665a6f94
Georgios Pardalis <georgpardal@gmail.com>
2019-05-09 13:27:09 +0300

    Added basic structure and pom.xml

---
+++ src/main/java/com/pardalis/betvictorassignment/BetvictorAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.pardalis.betvictorassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetvictorAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetvictorAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pardalis/betvictorassignment/BetvictorAssignmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pardalis.betvictorassignment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BetvictorAssignmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

803ee7330324a9a1da9b876423b2560da66399d5
Georgios Pardalis <georgpardal@gmail.com>
2019-05-12 14:15:10 +0300

    Changes in websocks

--- src/main/java/com/pardalis/betvictorassignment/validator/CommentDTOValidatorImpl.java
+++ src/main/java/com/pardalis/betvictorassignment/validator/CommentDTOValidatorImpl.java
@@ -3,10 +3,12 @@ package com.pardalis.betvictorassignment.validator;
 import com.pardalis.betvictorassignment.dto.CommentDTO;
 import com.pardalis.betvictorassignment.helper.enumeration.CommentDTOValidationError;
 import com.pardalis.betvictorassignment.helper.exception.CommentDTOValidationException;
+import org.springframework.stereotype.Component;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Component
 public class CommentDTOValidatorImpl implements CommentDTOValidator {
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
--- src/main/java/com/pardalis/betvictorassignment/web/CommentWebSocketImpl.java
+++ src/main/java/com/pardalis/betvictorassignment/web/CommentWebSocketImpl.java
@@ -2,28 +2,72 @@ package com.pardalis.betvictorassignment.web;
 
 import com.pardalis.betvictorassignment.dto.CommentDTO;
 import com.pardalis.betvictorassignment.dto.DisplayableCommentDTO;
+import com.pardalis.betvictorassignment.helper.enumeration.CommentAction;
+import com.pardalis.betvictorassignment.helper.exception.CommentDTOValidationException;
+import com.pardalis.betvictorassignment.model.ReviewableComment;
+import com.pardalis.betvictorassignment.receiver.MessageDestinations;
 import com.pardalis.betvictorassignment.service.CommentService;
+import com.pardalis.betvictorassignment.validator.CommentDTOValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CommentWebSocketImpl implements CommentWebSocket {
+    private final static Logger LOGGER = LoggerFactory.getLogger(CommentWebSocketImpl.class);
+
     @Autowired
     CommentService commentService;
+
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @Autowired
+    CommentDTOValidator commentDTOValidator;
 
     @Override
     @SubscribeMapping("/thread/comments")
     public List<DisplayableCommentDTO> onSubscribe() {
-        return commentService.findAllPersistedComments();
+        List<DisplayableCommentDTO> displayableCommentDTOs = new ArrayList<>();
+
+        try {
+            displayableCommentDTOs = commentService.findAllPersistedComments();
+        } catch (Exception e) {
+            LOGGER.warn("Could not send accepted comments to newly subscribed client", e);
+        }
+
+        return displayableCommentDTOs;
     }
 
     @Override
     @MessageMapping("/comment")
     public String onMessage(CommentDTO commentDTO) {
-        return null;
+        String commentAction = CommentAction.COMMENT_NOT_SENT.toString();
+
+        try {
+            commentDTOValidator.validateDTO(commentDTO);
+            jmsTemplate.convertAndSend(MessageDestinations.QUEUE_FOR_REVIEW, new ReviewableComment(
+                    commentDTO.getEmail(),
+                    commentDTO.getCommentText(),
+                    System.currentTimeMillis()
+            ));
+
+            commentAction = CommentAction.COMMENT_FOR_REVIEW.toString();
+        } catch (Exception e) {
+            LOGGER.warn("Could not process new comment", e);
+
+            if (e.getClass().equals(CommentDTOValidationException.class)) {
+                commentAction = e.getMessage();
+            }
+        }
+
+        return commentAction;
     }
 }

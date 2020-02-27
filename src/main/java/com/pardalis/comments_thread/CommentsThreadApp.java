0da6a88e9afe595eaf95aed67fd54ed26477510b
GPardalis <georgpardal@gmail.com>
2020-02-27 21:00:50 +0200

    Minor refactoring

--- src/main/java/com/pardalis/comments_thread/CommentsThreadApp.java
+++ src/main/java/com/pardalis/comments_thread/CommentsThreadApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BetvictorAssignmentApplication {
+public class CommentsThreadApp {
 	public static void main(String[] args) {
-		SpringApplication.run(BetvictorAssignmentApplication.class, args);
+		SpringApplication.run(CommentsThreadApp.class, args);
 	}
 }
--- src/main/java/com/pardalis/comments_thread/messaging/MessageDestinations.java
+++ src/main/java/com/pardalis/comments_thread/messaging/MessageDestinations.java
@@ -1,6 +1,6 @@
 package com.pardalis.comments_thread.messaging;
 
-public abstract class MessageDestinations {
-    public final static String QUEUE_FOR_REVIEW = "for_review";
-    public final static String QUEUE_ACCEPTED = "accepted";
+public interface MessageDestinations {
+    String QUEUE_FOR_REVIEW = "for_review";
+    String QUEUE_ACCEPTED = "accepted";
 }
